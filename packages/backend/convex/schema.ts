e20d955d7580d7da4203c34e613c31064703ea64
Elias Nau <contact@eliasnau.dev>
2025-08-14 00:08:09 +0200

    feat: add convex auth config

--- packages/backend/convex/schema.ts
+++ packages/backend/convex/schema.ts
@@ -2,8 +2,26 @@ import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export default defineSchema({
-  todos: defineTable({
-    text: v.string(),
-    completed: v.boolean(),
+  song: defineTable({
+    submitter: v.object({
+      name: v.string(),
+      email: v.string(),
+    }),
+
+    additionalInfo: v.optional(v.string()),
+
+    notes: v.optional(v.string()),
+    
+    accepted: v.boolean(),
+
+    submissionType: v.union(
+      v.literal("search"),
+      v.literal("youtube"),
+      v.literal("file")
+    ),
+
+    songSearch: v.optional(v.string()), 
+    youtubeUrl: v.optional(v.string()), 
+    songFile: v.optional(v.string()),
   }),
 });
